76bd2056aeef3ba829ea52f97ed0c5704a90f839
Louis Kirkham <louiskirkham@gmail.com>
2020-11-04 15:41:05 +0000

    added more tests to check running balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# account handles transaction data
+# account handles storage of transactions
 class Account
   attr_reader :transaction_history
 
@@ -14,23 +14,5 @@ class Account
 
   def withdraw(amount, date = Time.now)
     @transaction_history << [date, nil, '%.2f' % amount] unless amount <= 0
-  end
-
-  def print_statement
-    puts 'date || credit || debit || balance'
-    chronological_statement.reverse.map do |row|
-      puts row.join(' || ').squeeze(' ')
-    end
-  end
-
-  private
-
-  def chronological_statement
-    balance = 0
-    @transaction_history.map do |transaction|
-      credit, debit = [transaction[1].to_f, transaction[2].to_f]
-      balance += (credit - debit)
-      transaction << '%.2f' % balance
-    end
   end
 end
